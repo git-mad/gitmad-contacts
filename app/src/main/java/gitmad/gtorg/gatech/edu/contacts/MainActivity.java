17061b7210858497d4501bb3588cd31465dc3731
Jason Tu <title.kubo@gmail.com>
2018-12-09 17:08:33 -0500

    Initial commit.

---
+++ app/src/main/java/gitmad/gtorg/gatech/edu/contacts/MainActivity.java
@@ -0,0 +1,13 @@
+package gitmad.gtorg.gatech.edu.contacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
