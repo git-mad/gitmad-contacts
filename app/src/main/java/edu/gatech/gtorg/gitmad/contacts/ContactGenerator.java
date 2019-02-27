74fa183ed19908e18088b14c2012c4de5f43aa0b
Jason Tu <title.kubo@gmail.com>
2019-02-27 12:30:47 -0500

    Added ContactGenerator. Removed extra layout files

---
+++ app/src/main/java/edu/gatech/gtorg/gitmad/contacts/ContactGenerator.java
@@ -0,0 +1,36 @@
+package edu.gatech.gtorg.gitmad.contacts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.gatech.gtorg.gitmad.contacts.models.Attribute;
+import edu.gatech.gtorg.gitmad.contacts.models.Contact;
+
+public class ContactGenerator {
+    public List<Contact> generateContacts(int numContacts) {
+        List<Contact> toReturn = new ArrayList<>();
+        for (int i = 0; i < numContacts; i++) {
+            Contact contact = new Contact();
+
+            contact.setFirstName("FirstName" + i);
+            contact.setLastName("LastName" + i);
+            contact.setAttributes(generateAttributes(5));
+
+            toReturn.add(contact);
+        }
+        return toReturn;
+    }
+
+    public List<Attribute> generateAttributes(int numAttributes) {
+        List<Attribute> attributes = new ArrayList<>();
+        for (int i = 0; i < numAttributes; i++) {
+            Attribute attribute = new Attribute();
+
+            attribute.setKey("Key" + i);
+            attribute.setValue("Value" + i);
+
+            attributes.add(attribute);
+        }
+        return attributes;
+    }
+}
